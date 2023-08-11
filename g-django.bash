9262fe8cd973f0619ce2d2005f54bc48dc0610d8
geraldbahati <journeytoharvard@gmail.com>
2023-08-11 19:51:15 +0300

    setting up rest framework, simple-jwt

--- g-django.bash
+++ g-django.bash
@@ -19,10 +19,6 @@ djangorestframework-simplejwt
 pyyaml
 requests
 django-cors-headers
-channels-redis
-django_celery_results
-django_celery_beat
-celery
 django>=4.2.0
 selenium
 python-dotenv
@@ -47,6 +43,9 @@ DEBUG=True
 ALLOWED_HOSTS=*
 EOL
 
+# cd into project directory
+cd "$projectname" || exit
+
 # update STATIC_URL in settings.py
 sed -i "s/STATIC_URL = '\/static\/'/STATIC_URL = '\/static\/'\nSTATIC_ROOT = os.path.join(BASE_DIR, 'static')/g" "$projectname"/settings.py
 
@@ -64,7 +63,77 @@ sed -i "s/'django.contrib.staticfiles',/'django.contrib.staticfiles',\n    'rest
 sed -i "s/'django.contrib.staticfiles',/'django.contrib.staticfiles',\n    'rest_framework_simplejwt',/g" "$projectname"/settings.py
 
 # add REST_FRAMEWORK to settings.py
-sed 
+cat > temp_content.txt <<EOL
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES' : [
+        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework_simplejwt.authentication.JWTAuthentication',
+        # 'knox.auth.TokenAuthentication',
+
+    ],
+    'DEFAULT_PERMISSION_CLASSES' : [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly'
+    ], 
+
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100
+}
+EOL
+
+awk -v file=temp_content.txt '/DEFAULT_AUTO_FIELD = '\''django.db.models.BigAutoField'\''/ {print; while (getline < file) print; next} 1' "$projectname/settings.py" > "$projectname/settings.tmp"
+mv "$projectname/settings.tmp" "$projectname/settings.py"
+rm temp_content.txt
+
+
+# add SIMPLE_JWT to settings.py
+cat > temp_content.txt <<EOL
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=100),
+    "ROTATE_REFRESH_TOKENS": False,
+    "BLACKLIST_AFTER_ROTATION": False,
+    "UPDATE_LAST_LOGIN": False,
+
+    "ALGORITHM": "HS256",
+
+    "VERIFYING_KEY": "",
+    "AUDIENCE": None,
+    "ISSUER": None,
+    "JSON_ENCODER": None,
+    "JWK_URL": None,
+    "LEEWAY": 0,
+
+    "AUTH_HEADER_TYPES": ("Bearer",),
+    "AUTH_HEADER_NAME": "HTTP_AUTHORIZATION",
+    "USER_ID_FIELD": "id",
+    "USER_ID_CLAIM": "user_id",
+    "USER_AUTHENTICATION_RULE": "rest_framework_simplejwt.authentication.default_user_authentication_rule",
+
+    "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
+    "TOKEN_TYPE_CLAIM": "token_type",
+    "TOKEN_USER_CLASS": "rest_framework_simplejwt.models.TokenUser",
+
+    "JTI_CLAIM": "jti",
+
+    "SLIDING_TOKEN_REFRESH_EXP_CLAIM": "refresh_exp",
+    "SLIDING_TOKEN_LIFETIME": timedelta(minutes=5),
+    "SLIDING_TOKEN_REFRESH_LIFETIME": timedelta(days=1),
+
+    "TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainPairSerializer",
+    "TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSerializer",
+    "TOKEN_VERIFY_SERIALIZER": "rest_framework_simplejwt.serializers.TokenVerifySerializer",
+    "TOKEN_BLACKLIST_SERIALIZER": "rest_framework_simplejwt.serializers.TokenBlacklistSerializer",
+    "SLIDING_TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainSlidingSerializer",
+    "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
+}
+EOL
+
+awk -v file=temp_content.txt '/DEFAULT_AUTO_FIELD = '\''django.db.models.BigAutoField'\''/ {print; while (getline < file) print; next} 1' "$projectname/settings.py" > "$projectname/settings.tmp"
+mv "$projectname/settings.tmp" "$projectname/settings.py"
+rm temp_content.txt
+
+
+
 # setting up the packages
 # add corsheaders to INSTALLED_APPS
 sed -i "s/'django.contrib.staticfiles',/'django.contrib.staticfiles',\n    'corsheaders',/g" "$projectname"/settings.py
@@ -119,3 +188,39 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
     cat ../user/views.txt >"$projectname"/users/views.py
 fi
 
+# Prompt for setting up channels
+read -p "Set up channels for websockets? [y/n] " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    # add channels, daphne, channels-redis, django_celery_results, django_celery_beat, celery and django-channels to requirements.txt
+    # cd ..
+    cd ..
+    echo "channels" >>requirements.txt
+    echo "daphne" >>requirements.txt
+    echo "channels-redis" >>requirements.txt
+    echo "django_celery_results" >>requirements.txt
+    echo "django_celery_beat" >>requirements.txt
+    echo "celery" >>requirements.txt
+    echo "django-channels" >>requirements.txt
+
+    # install channels
+    pip install channels daphne channels-redis django_celery_results django_celery_beat celery django-channels
+
+    # cd back to project
+    cd "$projectname"
+
+    # add channels to INSTALLED_APPS
+    cat > temp_content.txt <<EOL
+        # Channels
+        'daphne',
+        'django_celery_results',
+        'django_celery_beat',
+        'celery',
+        'channels',
+EOL
+
+    awk -v file=temp_content.txt '/django.contrib.staticfiles/ {print; while (getline < file) print; next} 1' "$projectname/settings.py" > "$projectname/settings.tmp"
+    mv "$projectname/settings.tmp" "$projectname/settings.py"
+    rm temp_content.txt
+
+    # 
